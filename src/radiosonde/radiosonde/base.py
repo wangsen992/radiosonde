0185cd081b9222bf9d2ad7c6af4f523c44a10560
wangsen992 <wangsen992@gmail.com>
2020-10-19 23:28:53 +0000

    setup radiosonde composite structure

--- src/radiosonde/radiosonde/base.py
+++ src/radiosonde/radiosonde/base.py
@@ -1,6 +1,39 @@
+from abc import ABC, abstractmethod
+import pandas as pd
 
-
-
-class BaseRadiosonde:
+class BaseRadiosondeComponent(ABC):
     """Define an interface for working with  radiosonde data"""
-    pass
+
+    def is_list(self) -> bool:
+        
+        return False
+
+    @abstractmethod
+    def some_operation(self)  -> str:
+        """Defines the interface here"""
+        pass
+
+class BaseRadiosonde(BaseRadiosondeComponent):
+
+    def some_operation(self) -> str:
+
+        return "This is a BaseRadiosonde"
+
+class BaseRadiosondeList(BaseRadiosondeComponent):
+    """Composite for Radiosondes"""
+
+    def __init__(self) -> None:
+        self._children = []
+
+    def add(self, radiosonde: BaseRadiosondeComponent) -> None:
+        self._children.append(radiosonde)
+        
+    def remove(self, radiosonde: BaseRadiosondeComponent) -> None:
+        self._children.remove(radiosonde)
+
+    def is_list(self) -> bool:
+        return True
+
+    def some_operation(self) -> str:
+
+        return "This is a BaseRadiosodneList"
