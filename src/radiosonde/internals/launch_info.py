701ad1f4c9a1b0097d5abd8b33a699db7f6bd837
wangsen992 <wangsen992@gmail.com>
2020-10-27 14:50:10 +0000

    rename to sonde_datetime to avoid namespace clash with datetime

--- src/radiosonde/internals/launch_info.py
+++ src/radiosonde/internals/launch_info.py
@@ -1,8 +1,8 @@
 from typing import TypedDict
 
-from .datetime.base import BaseDatetime
+from .sonde_datetime.base import BaseDatetime
 from .gps import GeoLocation
 
 class LaunchInfo(TypedDict):
     launch_time : BaseDatetime
-    gps         : GeoLocation
+    launch_gps  : GeoLocation
