46425afa75d5cc0dd89451071facea0fb51f287c
wangsen992 <wangsen992@gmail.com>
2020-10-28 01:30:57 +0000

    correct __repr__

--- src/radiosonde/internals/gps.py
+++ src/radiosonde/internals/gps.py
@@ -3,8 +3,19 @@
 class GeoLocation:
 
     def __init__(self, lat, lon):
-        self.lat = lat
-        self.lon = lon
+        self.__lat = lat
+        self.__lon = lon
 
-    def __repr(self):
-        return f"{self.__class__.__name__}({self.lat}, {self.lon})"
+    @property
+    def lat(self):
+        return self.__lat
+
+    @property
+    def lon(self):
+        return self.__lon
+
+    def to_tuple(self):
+        return self.__lat, self.__lon
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.__lat}, {self.__lon})"
