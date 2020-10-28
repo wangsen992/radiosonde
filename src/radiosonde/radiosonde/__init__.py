cf64be9b49c854cae96c31c22a8f263c9699b785
wangsen992 <wangsen992@gmail.com>
2020-10-28 10:28:00 +0000

    routine commit

--- src/radiosonde/radiosonde/__init__.py
+++ src/radiosonde/radiosonde/__init__.py
@@ -1,2 +1,2 @@
-from .simple import SimpleDataFrameRadiosonde as Radiosonde
+from .simple import SimpleDataFrameRadiosonde as Readiosonde
 from .simple import SimpleDataFrameRadiosondeList as RadiosondeList
